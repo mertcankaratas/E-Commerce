3432ac98dac7bd8b19b6901a4db74fe3ecb55a1f
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-10-04 21:40:34 +0300

    Repository Design Pattern

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Product/ProductReadRepository.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class ProductReadRepository : ReadRepository<Product>, IProductReadRepository
+    {
+        public ProductReadRepository(ETicaretAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
