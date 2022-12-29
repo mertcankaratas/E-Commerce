15edab4bb9da826bb1206928cacb0f1221d6b719
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-12-29 19:47:58 +0300

    Authentication Adding

--- Presentation/ETicaretAPI.API/Controllers/UsersController.cs
+++ Presentation/ETicaretAPI.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Features.Commands.AppUser.CreateUser;
+using ETicaretAPI.Application.Features.Commands.AppUser.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,13 @@ namespace ETicaretAPI.API.Controllers
         {
             CreateUserCommandResponse response = await _mediator.Send(createUserCommandRequest);
             return Ok(response);
+        }
+
+        [HttpPost("[Action]")]
+        public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
+        {
+            LoginUserCommandResponse response = await _mediator.Send(loginUserCommandRequest);
+            return Ok();
         }
     }
 }
