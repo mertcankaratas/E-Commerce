3b2b2142fafb63bd12d40894a76da3382c1ec9af
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-01-24 19:26:13 +0300

    facebook login adding

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using ETicaretAPI.Application.Abstraction.Token;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
+{
+    public class FacebookLoginCommandHandler : IRequestHandler<FacebookLoginCommandRequest, FacebookLoginCommandResponse>
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        readonly ITokenHandler _tokenHandler;
+        readonly HttpClient _httpClient;
+
+        public FacebookLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler,IHttpClientFactory httpClientFactory)
+        {
+            _userManager = userManager;
+            _tokenHandler = tokenHandler;
+            _httpClient = httpClientFactory.CreateClient();
+        }
+
+        public async Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            string accessTokenResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/oauth/access_token\r\n  ?client_id={{your-app-id}}\r\n  &client_secret={{your-app-secret}}\r\n  &grant_type=client_credentials");
+            throw new NotImplementedException();
+        }
+    }
+}
