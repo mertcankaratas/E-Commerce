2164f810adaff359bb033d6825bd0eb20db356f4
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-01-27 17:58:44 +0300

    Facebook Login Added

---
+++ Core/ETicaretAPI.Application/DTOs/Facebook/FacebookUserAcessTokenValidationDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.DTOs.Facebook
+{
+    public class FacebookUserAcessTokenValidationDTO
+    {
+        [JsonPropertyName("data")]
+        public FacebookUserAccessTokenValidationData Data { get; set; }
+    }
+
+    public class FacebookUserAccessTokenValidationData
+    {
+        [JsonPropertyName("is_valid")]
+        public bool IsValid { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
+    }
+}
--- Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -1,10 +1,13 @@
 ﻿using ETicaretAPI.Application.Abstraction.Token;
+using ETicaretAPI.Application.DTOs;
+using ETicaretAPI.Application.DTOs.Facebook;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
@@ -15,7 +18,7 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
         readonly ITokenHandler _tokenHandler;
         readonly HttpClient _httpClient;
 
-        public FacebookLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler,IHttpClientFactory httpClientFactory)
+        public FacebookLoginCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, ITokenHandler tokenHandler, IHttpClientFactory httpClientFactory)
         {
             _userManager = userManager;
             _tokenHandler = tokenHandler;
@@ -24,8 +27,60 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
 
         public async Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
         {
-            string accessTokenResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/oauth/access_token\r\n  ?client_id={{your-app-id}}\r\n  &client_secret={{your-app-secret}}\r\n  &grant_type=client_credentials");
-            throw new NotImplementedException();
+            string accessTokenResponse = await _httpClient.GetStringAsync("");
+
+            FacebookAccessTokenDTO facebookAccessTokenResponse = JsonSerializer.Deserialize<FacebookAccessTokenDTO>(accessTokenResponse);
+
+            string userAccessTokenValidation = await _httpClient.GetStringAsync($"https://graph.facebook.com/debug_token?input_token={request.AuthToken}&access_token={facebookAccessTokenResponse.AccessToken}");
+
+
+            FacebookUserAcessTokenValidationDTO validation = JsonSerializer.Deserialize<FacebookUserAcessTokenValidationDTO>(userAccessTokenValidation);
+
+
+            if (validation.Data.IsValid)
+            {
+                string userInfoResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/me?fields=email,name&access_token={request.AuthToken}");
+
+                FacebookUserInfoResponse userInfo = JsonSerializer.Deserialize<FacebookUserInfoResponse>(userInfoResponse);
+
+                var info = new UserLoginInfo("FACEBOOK", validation.Data.UserId, "FACEBOOK");
+                Domain.Entities.Identity.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+                bool result = user != null;
+                if (user == null)
+                {
+                    user = await _userManager.FindByEmailAsync(userInfo.Email);
+                    if (user == null)
+                    {
+                        user = new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Email = userInfo.Email,
+                            UserName = userInfo.Email,
+                            nameSurname = userInfo.Name
+                        };
+                        var identityResult = await _userManager.CreateAsync(user);
+                        result = identityResult.Succeeded;
+                    }
+                }
+
+                if (result)
+                {
+                    await _userManager.AddLoginAsync(user, info); //AspNetUserLogins
+
+                    Token token = _tokenHandler.CreateAccessToken(5);
+                    return new()
+                    {
+                        Token = token
+                    };
+
+
+
+                }
+               
+
+            }
+            throw new Exception("Invalid external authentication.");
         }
     }
 }
