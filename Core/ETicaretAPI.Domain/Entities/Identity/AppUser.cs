452ad99ad4bc0a582b0e31077208770ccc684c16
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-02-16 11:00:36 +0300

    Refresh Token domain and migration

--- Core/ETicaretAPI.Domain/Entities/Identity/AppUser.cs
+++ Core/ETicaretAPI.Domain/Entities/Identity/AppUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,9 @@ namespace ETicaretAPI.Domain.Entities.Identity
     public class AppUser:IdentityUser<string>
     {
         public string nameSurname { get; set; }
+
+        public string RefreshToken { get; set; }
+
+        public DateTime? RefreshTokenEndDate { get; set; }
     }
 }
