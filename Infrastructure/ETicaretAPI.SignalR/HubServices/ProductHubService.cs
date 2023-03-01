bdea2a140910d9a6fe0467317036d6d3839909a4
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-03-01 19:10:44 +0300

    SignalR configuration

---
+++ Infrastructure/ETicaretAPI.SignalR/HubServices/ProductHubService.cs
@@ -0,0 +1,17 @@
+﻿using ETicaretAPI.Application.Abstraction.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.SignalR.HubServices
+{
+    public class ProductHubService : IProductHubService
+    {
+        public Task ProductAddedMessageAsync(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -7,6 +7,7 @@ using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPI.Infrastructure.Services.Storage.Azure;
 using ETicaretAPI.Infrastructure.Services.Storage.Local;
 using ETicaretAPI.Persistence;
+using ETicaretAPI.SignalR;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.HttpLogging;
@@ -26,6 +27,7 @@ builder.Services.AddPersistenceServices();
 builder.Services.AddInfrastructeServices();
 builder.Services.AddApplicationServices();
 builder.Services.AddStorage<AzureStorage>();
+builder.Services.AddSignalRServices();
 //builder.Services.AddStorage<LocalStorage>();
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
