8cb3852a7bd4b421880e292f762f73caa2622393
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-03-22 15:51:37 +0300

    Basket operation migration

--- Core/ETicaretAPI.Domain/Entities/Basket.cs
+++ Core/ETicaretAPI.Domain/Entities/Basket.cs
@@ -10,7 +10,7 @@ namespace ETicaretAPI.Domain.Entities
 {
     public class Basket:BaseEntity
     {
-        public Guid UserId { get; set; }
+        public string UserId { get; set; }
         public AppUser User { get; set; }
 
         public ICollection<BasketItem> BasketItems  { get; set; }
