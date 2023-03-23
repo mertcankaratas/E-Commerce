82ac6dffc370bce0fb2b7367c2394c47bd386a15
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-03-23 17:17:04 +0300

    Add basket command Adding

---
+++ Core/ETicaretAPI.Application/Features/Commands/Basket/AddItemToBasket/AddItemToBasketCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Basket.AddItemToBasket
+{
+    public class AddItemToBasketCommandHandler : IRequestHandler<AddItemToBasketCommandRequest, AddItemToBasketCommandResponse>
+    {
+        public Task<AddItemToBasketCommandResponse> Handle(AddItemToBasketCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/ETicaretAPI.Application/Features/Commands/Basket/AddItemToBasket/AddItemToBasketCommandRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Basket.AddItemToBasket
+{
+    public class AddItemToBasketCommandRequest:IRequest<AddItemToBasketCommandResponse>
+    {
+    }
+}
