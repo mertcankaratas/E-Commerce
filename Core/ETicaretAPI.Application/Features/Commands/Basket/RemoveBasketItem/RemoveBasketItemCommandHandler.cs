55e99b4a0675409bbadfff23b005e0969ea70a8e
Mertcan Karataş <can.mert.karatas@gmail.com>
2023-03-23 17:20:06 +0300

    RemoveBasket command Adding

---
+++ Core/ETicaretAPI.Application/Features/Commands/Basket/RemoveBasketItem/RemoveBasketItemCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Basket.RemoveBasketItem
+{
+    public class RemoveBasketItemCommandHandler : IRequestHandler<RemoveBasketItemCommandRequest, RemoveBasketItemCommandResponse>
+    {
+        public Task<RemoveBasketItemCommandResponse> Handle(RemoveBasketItemCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
